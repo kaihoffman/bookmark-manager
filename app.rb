bf7c58cb024189d72d719cb25e6bb3e8c50a118a
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-01-30 18:36:14 +0000

    Tweaked feature test for adding bookmarks

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
+  enable :sessions
 
   get '/' do
     "Hi and welcome to the Bookmark Manager app"
@@ -10,6 +11,13 @@ class BookmarkManager < Sinatra::Base
   get '/bookmarks' do
     @bookmark = Bookmark.all
     erb(:bookmarks)
+  end
+
+  post '/add-bookmark' do
+    url = params['url']
+    title = params['title']
+    Bookmark.create_new_bookmark(url, title)
+    redirect '/bookmarks'
   end
 
   run! if app_file == $0
